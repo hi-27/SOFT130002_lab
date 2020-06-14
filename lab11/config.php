a0d7fdb62cdef025bdb1c7785e24e25619218ffe
hi-27 <61503485+hi-27@users.noreply.github.com>
2020-06-14 10:12:22 +0800

    lab11

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'Login');
+define('DBUSER', 'winter');
+define('DBPASS', 'abc');
+define('DBCONNSTRING','mysql:host=localhost;dbname=Login');
+?>
